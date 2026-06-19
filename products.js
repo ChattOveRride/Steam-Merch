791ef169305410291d0d42ec6a5a27693a0ed97e
CrasheR <s.4lcala@gmail.com>
2026-06-18 21:16:22 -0500

    Add files via upload

---
+++ products.js
@@ -0,0 +1,322 @@
+    const productdata = {
+    '1': {
+        name: 'Sans Plush',
+        game: 'UNDERTALE',
+        price: 'Mex$684',
+        desc: 'its sans. sans the skeleton. <br>This sans plush is 10 inches tall (seated) and comes with a couple of extra eyes, for some reason.',
+        },
+    '2': {
+        name: 'Sans Hoodie',
+        game: 'UNDERTALE',
+        price: 'Mex$800',
+        desc: 'Along the way, some fuzzy folk decided they liked the cold and set up camp in Snowdin.<br><br>This official UNDERTALE hoodie is a fully custom design—everything from the cord pulls to the fabric liner was made especially for it. The body of the jacket is 100% cotton, and the interior of the hood is sherpa fleece. The things you can do with a college education!',
+        },
+    '3': {
+        name: 'Annoying Dog Lapel Pin',
+        game: 'UNDERTALE',
+        price: 'Mex$220',
+        desc: 'This 1" cloisonné Annoying Dog lapel pin will stick to you no matter what you do, unless you pull the pin back off.',
+        },
+    '4': {
+        name: 'Ralsei Plush',
+        game: 'DELTARUNE',
+        price: 'Mex$220',
+        desc: 'Hey can you STOP mumbling into your hat for a sec? Can barely hear ya dude.<br><br>This official DELTARUNE plush was designed by Saber Murphy based on a template created by Eyes5. Its about seven inches tall when seated and is not wearing a hat.',
+        },
+    '5': {
+        name: 'Pipis Ornament',
+        game: 'DELTARUNE',
+        price: 'Mex$171',
+        desc: '’ ’ The Traditional ’ ’<br><br>This official DELTARUNE ornament is made from felt and about 3 inches long. It hangs from a red ribbon. ',
+        },
+    '6': {
+        name: 'Darkheart Pin',
+        game: 'DELTARUNE',
+        price: 'Mex$220',
+        desc: 'There is a prophecy. The Angel... The One Who Has Seen The Surface... They will return. And the underground will go empty.<br><br>This glow-in-the-dark enamel pin was designed by Jon Kay.',
+        },
+    '7': {
+        name: 'Hang in There Keychain',
+        game: 'PEAK',
+        price: 'Mex$260',
+        desc: 'Give your friends a hand.<br><br>This official PEAK keychain was designed by Melora Mylin. Its made from three independent acrylic charms connected to a keychain with the PEAK logo and a small lobster clasp. Its about 3.4 inches long.',
+        },
+    '8': {
+        name: 'Up to the Peak',
+        game: 'PEAK',
+        price: 'Mex$680',
+        desc: 'Take some friends to the summit.<br><br>This official PEAK shirt was designed by Melora Mylin and printed front and back by Forward on soft, 100% cotton t-shirts from Canvas and District. ',
+        },
+    '9': {
+        name: 'BINGBONG Airlines Hat',
+        game: 'PEAK',
+        price: 'Mex$530',
+        desc: 'Scan your boarding pass, let your friends know where youre sitting, and assume the crash position.<br><br>This official PEAK hat was designed by Tony Kuchar. Its an unstructured strapback cap, embroidered front and back with the BINGBONG Airlines and PEAK logos. ',
+        },
+    '10': {
+        name: 'Haunted Robe',
+        game: 'REPO',
+        price: 'Mex$400',
+        desc: '‘Don’t forget to hide!’<br><br>Keep quiet! You don’t want to alert Robe! Oh no, it’s too late! Lurking in at 4.6 inches tall with hollow eyes and a horrifying skeletal mouth that has sharp teeth on the lower jaw, clawed grey hands reach out from their black body to grab onto the frightened red semibot at their front as a blue semibot hides behind them holding a grey flashlight.',
+        },
+    '11': {
+        name: 'Pin Set',
+        game: 'REPO',
+        price: 'Mex$300',
+        desc: 'Quick, grab the good stuff and run! Our brand new R.E.P.O. pin set is the most valuable thing you can bring back to the extraction point! With 6 unique enamel pins that each measure at 2 inches with a shiny, high-quality finish, the collection sees The Huntsman with a rifle, a very toothy crying face, a screaming voice chat, a red semibot that’s about to be devoured, The Robe and a blue semibot in a cup! Perfect as decoration on a corkboard or showing off on your tote bag, backpack, fanny pack, jacket or wherever your heart desires, these pins are sure to turn heads and catch the eyes of anyone you pass, letting them know who’s been extracting goods!',
+        },
+
+    '12': {
+        name: 'For the Views',
+        game: 'CONTENT-WARNING',
+        price: 'Mex$960',
+        desc: '',
+        },
+    '13': {
+        name: 'Logo Cap',
+        game: 'CONTENT-WARNING',
+        price: 'Mex$300',
+        desc: '',
+        },
+    '14': {
+        name: 'Group Photo Poster',
+        game: 'CONTENT-WARNING',
+        price: 'Mex$250',
+        desc: '',
+        },
+    '15': {
+        name: 'Hollow Knight: Silksong Mini Figurines',
+        game: 'Silksong',
+        price: 'Mex$2100',
+        desc: 'Look here at the marvelous beasts and bugs, all sculpted so beautifully!<br><br>These official Hollow Knight: Silksong mini figs were modeled by Alex Colvin. All eight are also available separately. ',
+        },
+    '16': {
+        name: 'Intrepid Pursuer',
+        game: 'Silksong',
+        price: 'Mex$684',
+        desc: 'Climb higher, little spider. I want to savour your struggle just a little longer!<br><br>This official Hollow Knight: Silksong shirt was designed by Studio Ilustrata and printed by Forward on soft, poly-cotton shirts from Canvas and District. It`s part of a series including Wandering Knight (The Knight) and Nimble Protector (Hornet).',
+        },
+    '17': {
+        name: 'Pharloom`s Champion Poster',
+        game: 'Silksong',
+        price: 'Mex$450',
+        desc: '<p style="text-align: center;">They see your beauty, so frail and fine,<br>They see your peace, woven of faith and toil,<br>They forget your heart, bound in slumber and servitude,<br>When you wake they shall see your truth,<br>A beast`s nature bare to all.</p>',
+        },
+    '18': {
+        name: 'Sticky Note Booklet',
+        game: 'OMORI',
+        price: 'Mex$260',
+        desc: 'This official OMORI sticky note booklet was designed for Fangamer Japan by Ikuno Ohashi.<br><br>It unfolds to reveal 20 sheets each of 12 different types of sticky note. ',
+        },
+    '19': {
+        name: 'OMORI Character Pin Sets',
+        game: 'OMORI',
+        price: 'Mex$300+',
+        desc: 'These official OMORI character pin sets were designed by ADE for Fangamer Japan. Pick each two-pin set up separately or save when you buy them all together.',
+        },
+    '20': {
+        name: 'Headspace Desk Mat',
+        game: 'OMORI',
+        price: 'Mex$530',
+        desc: '(Waiting for something to happen?)<br><br>This official OMORI desk mat was designed by ADE for Fangamer Japan. It measures 35x16 inches across, with a 3 mm rubber backing.',
+        },
+    '21': {
+        name: 'Acrylic Stand',
+        game: 'Needy-Girl-Overdose',
+        price: 'Mex$200',
+        desc: '. . . ♡ ♰ ♡ . . .',
+        },
+    '22': {
+        name: 'Comic Book',
+        game: 'Needy-Girl-Overdose',
+        price: 'Mex$500',
+        desc: 'A comic book and merchandise set featuring the popular comic book adaptation "New Girl Overdose: Absolute Revolutionary Sickness Incident Midnight Streaming Love❤️ †100 Days of Ame-chan and Pi`s Despair†"',
+        },
+    '23': {
+        name: 'Card Collection',
+        game: 'Needy-Girl-Overdose',
+        price: 'Mex$600',
+        desc: ' ■Nidiga Arcana Card Collection 2<br> The second Arcana card featuring Nidiga characters is now available!<br> The new design makes collecting even more fun.<br> A collectible item that you`ll want to display side by side!',
+        },
+    '24': {
+        name: 'Niko Plush',
+        game: 'OneShot',
+        price: 'Mex$600',
+        desc: 'See? I have the sun!<br>This official OneShot plush was designed by Nightmargin. Niko is 6.5 inches tall and features a wide, bendable brim, and includes a plush sun, which attaches to Niko`s hands with magnets.',
+        },
+    '25': {
+        name: 'Niko Scarf',
+        game: 'OneShot',
+        price: 'Mex$450',
+        desc: 'It could get pretty chilly at the top of an impossibly tall tower.<br><br>This official OneShot scarf was designed by NightMargin. It`s a fleece scarf measuring about 70 inches long, with six knotted tassels on each side and an embroidered Niko design at one end.',
+        },
+    '26': {
+        name: 'World Machine Hoodie',
+        game: 'OneShot',
+        price: 'Mex$1300',
+        desc: 'It was never meant to develop a mind of its own.<br><br>This official OneShot hoodie was designed by Nightmargin. It s printed in four locations by Forward on a District 1103 hoodie and includes a custom-minted zipper pull. (Sizes 5X-6X are on Independent IND4000Z.)',
+        },
+    '27': {
+        name: 'Beer Stein',
+        game: 'Age-of-Empires',
+        price: 'Mex$700',
+        desc: '“Building an empire is thirsty work; stay hydrated with this ceramic stein!”',
+        },
+    '28': {
+        name: 'King of Scouts',
+        game: 'Age-of-Empires',
+        price: 'Mex$1200',
+        desc: '“This scout sheep sweater will keep you cozy during those winter campaigns.”',
+        },
+    '29': {
+        name: 'Wololo Cap',
+        game: 'Age-of-Empires',
+        price: 'Mex$450',
+        desc: '"Wear your Age: Wololo cap with pride"',
+        },
+    };
+
+
+const Indexes = Array.from({ length: 50 }, (_, i) => i + 1);
+
+Indexes.forEach((i) => {
+const data = productdata[i];
+  const prod = document.createElement("div");
+  prod.classList.add("products-cont");
+  prod.classList.add(data.game);
+  prod.addEventListener('click', function() {openPage(i);}, false);
+
+  const image = document.createElement("img");
+  image.src = `assets/products/product-${i}.png`;
+  image.alt = "desc ${i}";
+  
+  const name = document.createElement("h2");
+  name.innerHTML = data.name;
+  
+  const game = document.createElement("h4");
+  game.innerHTML = data.game;
+  
+  const price = document.createElement("div");
+  price.innerHTML = data.price;
+  price.classList.add("price");
+
+  prod.appendChild(image);
+  prod.appendChild(name);
+  prod.appendChild(game);
+  prod.appendChild(price);
+  productswrapper.appendChild(prod);
+});
+
+function fall(){
+document.querySelectorAll('.products-cont').forEach(c => c.style.display = 'flex');
+document.querySelectorAll('.selected').forEach(c => c.classList.remove("selected"));
+document.getElementById('fall').classList.add("selected");
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
+
+function fundertale(){
+document.querySelectorAll('.products-cont').forEach(c => c.style.display = 'none');
+document.querySelectorAll('.UNDERTALE').forEach(b => b.style.display = 'flex');
+document.querySelectorAll('.selected').forEach(c => c.classList.remove("selected"));
+document.getElementById('fundertale').classList.add("selected");
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
+
+function fdeltarune(){
+document.querySelectorAll('.products-cont').forEach(c => c.style.display = 'none');
+document.querySelectorAll('.DELTARUNE').forEach(b => b.style.display = 'flex');
+document.querySelectorAll('.selected').forEach(c => c.classList.remove("selected"));
+document.getElementById('fdeltarune').classList.add("selected");
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
+
+function fpeak(){
+document.querySelectorAll('.products-cont').forEach(c => c.style.display = 'none');
+document.querySelectorAll('.PEAK').forEach(b => b.style.display = 'flex');
+document.querySelectorAll('.selected').forEach(c => c.classList.remove("selected"));
+document.getElementById('fpeak').classList.add("selected");
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
+
+function frepo(){
+document.querySelectorAll('.products-cont').forEach(c => c.style.display = 'none');
+document.querySelectorAll('.REPO').forEach(b => b.style.display = 'flex');
+document.querySelectorAll('.selected').forEach(c => c.classList.remove("selected"));
+document.getElementById('frepo').classList.add("selected");
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
+
+function fcontent(){
+document.querySelectorAll('.products-cont').forEach(c => c.style.display = 'none');
+document.querySelectorAll(".CONTENT-WARNING").forEach(b => b.style.display = 'flex');
+document.querySelectorAll('.selected').forEach(c => c.classList.remove("selected"));
+document.getElementById('fcontent').classList.add("selected");
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
+
+function fsilksong(){
+document.querySelectorAll('.products-cont').forEach(c => c.style.display = 'none');
+document.querySelectorAll('.Silksong').forEach(b => b.style.display = 'flex');
+document.querySelectorAll('.selected').forEach(c => c.classList.remove("selected"));
+document.getElementById('fsilksong').classList.add("selected");
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
+
+function fomori(){
+document.querySelectorAll('.products-cont').forEach(c => c.style.display = 'none');
+document.querySelectorAll(".OMORI").forEach(b => b.style.display = 'flex');
+document.querySelectorAll('.selected').forEach(c => c.classList.remove("selected"));
+document.getElementById('fomori').classList.add("selected");
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
+
+function fneedy(){
+document.querySelectorAll('.products-cont').forEach(c => c.style.display = 'none');
+document.querySelectorAll(".Needy-Girl-Overdose").forEach(b => b.style.display = 'flex');
+document.querySelectorAll('.selected').forEach(c => c.classList.remove("selected"));
+document.getElementById('fneedy').classList.add("selected");
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
+
+function foneshot(){
+document.querySelectorAll('.products-cont').forEach(c => c.style.display = 'none');
+document.querySelectorAll(".OneShot").forEach(b => b.style.display = 'flex');
+document.querySelectorAll('.selected').forEach(c => c.classList.remove("selected"));
+document.getElementById('foneshot').classList.add("selected");
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
+
+function fage(){
+document.querySelectorAll('.products-cont').forEach(c => c.style.display = 'none');
+document.querySelectorAll(".Age-of-Empires").forEach(b => b.style.display = 'flex');
+document.querySelectorAll('.selected').forEach(c => c.classList.remove("selected"));
+document.getElementById('fage').classList.add("selected");
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
+
+function openPage(i) {
+    const data = productdata[i];
+    document.getElementById("page-back").style.display = "block";
+    document.getElementById("page-img").src = `assets/products/product-${i}.png`;
+    document.getElementById("page-img").alt = `desc ${i}`;
+
+    document.getElementById("page-name").innerHTML = data.name;
+    document.getElementById("page-price").innerHTML = data.price;
+    document.getElementById("page-desc").innerHTML = data.desc;
+    document.getElementById("page-logo").src = "assets/logos/"+data.game+".png";
+}
+
+function closePage() {
+  document.getElementById("page-back").style.display = "none";
+}
